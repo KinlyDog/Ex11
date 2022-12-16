81bda63d9647389c6fb2f7d3262076fcbd520829
Andrey Otryaskin <kinlydog@gmail.com>
2022-12-16 22:42:46 +1000

    Update project

--- src/Level1.java
+++ src/Level1.java
@@ -1,9 +1,9 @@
 public class Level1 {
     public static boolean LineAnalysis(String line) {
-        int strLen = line.length();
+        final int STRING_LENGTH = line.length();
 
-        for (int i = 0; i < strLen / 2; i++) {
-            if (line.charAt(i) != line.charAt(strLen - 1 - i)) {
+        for (int i = 0; i < STRING_LENGTH / 2; i++) {
+            if (line.charAt(i) != line.charAt(STRING_LENGTH - 1 - i)) {
                 return false;
             }
         }
